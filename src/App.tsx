eb4f7647c4f4e66320c2396032780f4d93cb457d
Sushant Patil <sushant.patiljoshsoftware.com>
2023-04-03 18:14:09 +0530

    App changed for run

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from 'react-query';
 
 import RootNavigator from './app/navigation/RootNavigator';
-import CustomTabView from './app/navigation/customTabView';
 
 import UserContext, {UserData} from './app/context/user.context';
 
