90e90bfeca8f5b89355a3667e90f992d4ccba765
Chetan-Satpute-Josh <124863404+Chetan-Satpute-Josh@users.noreply.github.com>
2023-06-20 15:22:18 +0530

    Added background image on login screen (#91)

--- src/app/screens/LoginScreen/index.tsx
+++ src/app/screens/LoginScreen/index.tsx
@@ -1,5 +1,5 @@
-import React, {useCallback, useEffect} from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useEffect, useCallback} from 'react';
+import {ImageBackground, SafeAreaView, StyleSheet, View} from 'react-native';
 
 import Button from '../../components/button';
 import {useLogin} from './login.hooks';
@@ -7,6 +7,7 @@ import {useLogin} from './login.hooks';
 import {googleSignIn} from '../../services/auth/google.auth';
 
 import {JoshLogo} from '../../constant/icons';
+import boxBackgroundImage from '../../../assets/images/boxBackground.png';
 
 const LoginScreen = () => {
   const {mutate, isLoading} = useLogin();
@@ -23,20 +24,22 @@ const LoginScreen = () => {
   }, [googleSignInHandler]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.logoContainer}>
-        <JoshLogo />
-      </View>
-      <View>
-        <Button
-          type="primary"
-          title="Login With Google"
-          disabled={isLoading}
-          onPress={googleSignInHandler}
-          isLoading={isLoading}
-        />
-      </View>
-    </SafeAreaView>
+    <ImageBackground source={boxBackgroundImage} style={styles.imageContainer}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.logoContainer}>
+          <JoshLogo />
+        </View>
+        <View>
+          <Button
+            type="primary"
+            title="Login With Google"
+            disabled={isLoading}
+            onPress={googleSignInHandler}
+            isLoading={isLoading}
+          />
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
@@ -46,6 +49,9 @@ const styles = StyleSheet.create({
     padding: 16,
     justifyContent: 'space-between',
     paddingVertical: 90,
+  },
+  imageContainer: {
+    flex: 1,
   },
   logoContainer: {
     paddingVertical: 90,
