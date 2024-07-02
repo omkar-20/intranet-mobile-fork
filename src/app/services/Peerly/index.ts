44015ea918ecf9aad27c28a34f771a6c3f3fe9d9
akshay-ajit-doiphode <akshay.doiphode@joshsoftware.com>
2024-07-02 15:05:36 +0530

    Peerly component added. Added mock API for peerlyLogin check

--- src/app/services/Peerly/index.ts
+++ src/app/services/Peerly/index.ts
@@ -3,9 +3,18 @@ import {apiCall} from '../api';
 import {PEERLY_LOGIN_ROUTE} from '../../constant/apiRoutes';
 import {IPeerlyLoginResponse} from './types';
 
+// export const loginPeerlySystemRequest = async () => {
+//   const response = await apiCall<any, IPeerlyLoginResponse>({
+//     method: 'POST',
+//     url: PEERLY_LOGIN_ROUTE,
+//   });
+
+//   return response;
+// };
+
 export const loginPeerlySystemRequest = async () => {
   const response = await apiCall<any, IPeerlyLoginResponse>({
-    method: 'POST',
+    method: 'GET',
     url: PEERLY_LOGIN_ROUTE,
   });
 
