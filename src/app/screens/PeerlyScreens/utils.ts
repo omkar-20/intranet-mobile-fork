3cf4368fc8f32b9fe3a23ad5ff530e7b75c6de71
amar-josh <amar.jadhav@joshsoftware.com>
2024-07-22 18:36:26 +0530

    Add util functin for display date base on passing date format

--- src/app/screens/PeerlyScreens/utils.ts
+++ src/app/screens/PeerlyScreens/utils.ts
@@ -1,10 +1,24 @@
 import moment from 'moment';
 
-export const timeFromNow = (time: number): string => {
-  // Validate timestamp
+const validateDate = (time: number): boolean => {
   if (typeof time !== 'number' || isNaN(time) || time <= 0) {
+    return true;
+  }
+  return false;
+};
+
+export const timeFromNow = (time: number): string => {
+  if (validateDate(time)) {
     return 'Invalid time';
   }
-
   return moment(time).fromNow();
+};
+
+export const dateFormat = (time: number, format: string): string => {
+  if (validateDate(time)) {
+    return 'Invalid Date';
+  }
+  const date = moment.unix(time);
+  const formattedDate = date.format(format);
+  return formattedDate;
 };
