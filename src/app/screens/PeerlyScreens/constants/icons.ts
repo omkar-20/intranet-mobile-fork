b2786fe142a70565eb4812e605a7b62f6f0b7fdb
amar-josh <amar.jadhav@joshsoftware.com>
2024-07-22 18:36:51 +0530

    Add Icons into icon constant file

---
+++ src/app/screens/PeerlyScreens/constants/icons.ts
@@ -0,0 +1,27 @@
+import PlatinumIcon from '../../../../assets/peerly/svg/platinum.svg';
+import GoldIcon from '../../../../assets/peerly/svg/gold.svg';
+import SilverIcon from '../../../../assets/peerly/svg/silver.svg';
+import BronzeIcon from '../../../../assets/peerly/svg/bronze.svg';
+import TrustIcon from '../../../../assets/peerly/svg/trust.svg';
+import TechnicalIcon from '../../../../assets/peerly/svg/technical.svg';
+import RespectIcon from '../../../../assets/peerly/svg/respect.svg';
+import EthicsIcon from '../../../../assets/peerly/svg/ethics.svg';
+import CustFocusIcon from '../../../../assets/peerly/svg/custfocus.svg';
+import SuccessIcon from '../../../../assets/peerly/svg/Vector.svg';
+import InfoIcon from '../../../../assets/peerly/svg/info.svg';
+import StarIcon from '../../../../assets/svg/star.svg';
+
+export {
+  PlatinumIcon,
+  GoldIcon,
+  SilverIcon,
+  BronzeIcon,
+  TrustIcon,
+  TechnicalIcon,
+  RespectIcon,
+  EthicsIcon,
+  CustFocusIcon,
+  SuccessIcon,
+  InfoIcon,
+  StarIcon,
+};
