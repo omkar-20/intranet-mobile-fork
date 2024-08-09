f1b36dd2404517eeaa820cd3bf7e6eeb8a31ebb6
SamnitPatil9882 <96126478+SamnitPatil9882@users.noreply.github.com>
2024-08-09 19:17:37 +0530

    [feature/notification] notification added with notification token (#140)

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+  });
+
+messaging()
+ .subscribeToTopic('peerly')
+ .then(() => console.log('Subscribed to peerly topic!'));
 
 AppRegistry.registerComponent(appName, () => App);
