4c987f8f1208feae3b157c58dd663fbbf2d0ce83
omkar-20 <bhosale1119omkar@gmail.com>
2025-03-10 11:34:38 +0530

    Added dropdown to report appreciation dialog with default reasons

--- src/app/Peerly/components/ObjectionModal.tsx
+++ src/app/Peerly/components/ObjectionModal.tsx
@@ -1,13 +1,15 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useState} from 'react';
 import {
   Modal,
   View,
   Text,
   StyleSheet,
-  TextInput,
   TouchableWithoutFeedback,
+  Dimensions,
 } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
 import Button from './button/button';
+import colors from '../constants/colors';
 
 interface CenteredModalProps {
   visible: boolean;
@@ -17,7 +19,7 @@ interface CenteredModalProps {
   reason: string;
   isLoading: boolean;
 }
-
+const {height: SCREEN_HEIGHT} = Dimensions.get('window');
 const ObjectionModal: React.FC<CenteredModalProps> = ({
   visible,
   onClose,
@@ -26,9 +28,16 @@ const ObjectionModal: React.FC<CenteredModalProps> = ({
   reason,
   isLoading,
 }) => {
-  const handleReason = (value: string) => {
-    setReason(value);
-  };
+  const [selectedReason, setSelectedReason] = useState(reason);
+
+  const reasonOptions = [
+    {label: 'Insufficient Description', value: 'Insufficient Description'},
+    {label: 'Values Mismatch', value: 'Values Mismatch'},
+    {label: 'Inappropriate appreciation', value: 'Inappropriate appreciation'},
+    {label: 'Part of routine roles and responsibilties', value: 'Part of routine roles and responsibilties'},
+    {label: 'Action is not from current quarter ', value: 'Action is not from current quarter '},
+    {label: 'Others', value: 'Others'},
+  ];
 
   const isReasonEmpty = useMemo(() => {
     if (!reason.trim().length) {
@@ -54,12 +63,21 @@ const ObjectionModal: React.FC<CenteredModalProps> = ({
               <Text style={styles.modalText}>
                 HR team will contact you shortly.
               </Text>
-              <TextInput
-                style={styles.description}
-                placeholder="Enter you text here"
-                onChangeText={handleReason}
-                value={reason}
-                multiline
+              <Dropdown
+                style={styles.dropdown}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                containerStyle={styles.containerStyle}
+                data={reasonOptions}
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                placeholder="Select a reason"
+                value={selectedReason}
+                onChange={item => {
+                  setSelectedReason(item.value);
+                  setReason(item.value);
+                }}
               />
               <View style={styles.buttonContainer}>
                 <Button
@@ -107,16 +125,27 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingHorizontal: 40,
   },
-  description: {
-    textAlignVertical: 'top',
-    borderRadius: 12,
+  dropdown: {
+    height: 50,
+    width:200,
+    borderColor: colors.MEDIUM_GRAY,
+    borderRadius: 4,
+    paddingHorizontal: 8,
     borderWidth: 1,
-    height: '40%',
-    width: '90%',
-    borderColor: 'transparent',
-    marginBottom: 16,
-    backgroundColor: 'white',
-    padding: 10,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  containerStyle: {
+    borderRadius: 10,
+    paddingBottom: 0,
+    paddingVertical: 0,
+    marginVertical: 0,
+    maxHeight: SCREEN_HEIGHT / 4.5,
+    width:'50%',
   },
 });
 
